661f18da0527ea032277ad9afc73fc786e5d8984
juanbautistacrespialomar <juanbautistacrespialomar@gmail.com>
2026-07-03 14:33:14 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -9,7 +9,7 @@
    │  A cada persona le va a aparecer el cartel "Hay versión nueva". │
    └───────────────────────────────────────────────────────────────┘ */
 
-const VERSION = "v17";
+const VERSION = "v18";
 const CACHE = "compartimos-" + VERSION;
 const ASSETS = [
   "./",
